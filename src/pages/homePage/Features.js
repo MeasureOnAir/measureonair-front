30160a7867cd34db7034969364cb52b0cf0cd1d4
Suboda Dabarera <SubodaDabarera99@gmail.com>
2023-01-08 02:43:42 +0530

    color changed

--- src/pages/homePage/Features.js
+++ src/pages/homePage/Features.js
@@ -90,15 +90,15 @@ const Features = () => {
   return (
     <div>
       {/* mobile view */}
-      <section className="lg:col-span-7 block md:hidden">
-        <h2 className="mx-4 mt-0 mb-8 text-3xl font-extrabold tracking-tight sm:text-4xl text-gray-900">
+      <section className="lg:col-span-7 block md:hidden ">
+        <h2 className="mx-4 mt-11 mb-8 text-3xl font-extrabold tracking-normal sm:text-4xl text-gray-900 dark:text-gray-200">
           Features
         </h2>
         <ul role="list" className="">
           {features.map((feature, featureIdx) => (
             <li
               key={feature.id}
-              className="flex py-4 px-1 sm:py-6 m-3 my-5 rounded-lg bg-gray-100 shadow-lg"
+              className="flex py-4 px-1 sm:py-6 m-3 my-5 rounded-lg bg-gray-100 dark:bg-secondary-gray700 shadow-lg"
             >
               {featureIdx % 2 == 0 ? (
                 <>
@@ -114,16 +114,15 @@ const Features = () => {
                     <div className="relative grid justify-center items-center">
                       <div>
                         <h3 className="text-center">
-                          <a
-                            href={feature.path}
-                            className="text-base sm:text-lg font-medium text-gray-900 hover:text-gray-800"
+                          <div
+                            className="text-base sm:text-lg font-bold text-gray-900 dark:text-gray-200 "
                           >
                             {feature.title}
-                          </a>
+                          </div>
                         </h3>
 
                         <div className="mt-1 text-center">
-                          <p className="text-base sm:text-base text-gray-500">
+                          <p className="text-base sm:text-base text-gray-500 dark:text-gray-300">
                             {feature.description}
                           </p>
                         </div>
@@ -139,14 +138,14 @@ const Features = () => {
                         <h3 className="text-center">
                           <a
                             href={feature.path}
-                            className="text-base sm:text-lg font-medium text-gray-900 hover:text-gray-800"
+                            className="text-base sm:text-lg font-bold text-gray-900 dark:text-gray-200"
                           >
                             {feature.title}
                           </a>
                         </h3>
 
                         <div className="mt-1 flex  text-center">
-                          <p className="text-sm sm:text-base text-gray-500">
+                          <p className="text-sm sm:text-base text-gray-500 dark:text-gray-300">
                             {feature.description}
                           </p>
                         </div>
@@ -168,17 +167,18 @@ const Features = () => {
       </section>
 
       {/* desktop view - start */}
-      <div className="hidden md:block">
+      <div className="hidden md:block dark:bg-transparent">
         <div className="max-w-2xl mx-auto py-5 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="flex items-center justify-between space-x-4 mb-12">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl text-gray-900 ">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl text-gray-900 dark:text-gray-200">
               Features
             </h2>
           </div>
-          <div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-3">
+          <div className="mt-6 grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-3">
             {features.map((feature) => (
-              <div key={feature.id} className="relative group ">
-                <div className="aspect-w-4 aspect-h-3 grid justify-center rounded-lg overflow-hidden bg-gray-100 drop-shadow-lg">
+              <div className="grid justify-center">
+              <div key={feature.id} className="relative group">
+                <div className="aspect-w-4 aspect-h-3 py-4 w-72 grid justify-center rounded-lg overflow-hidden bg-gray-100 drop-shadow-lg dark:bg-secondary-gray700">
                   <div className="grid justify-center">
                     <img
                       src={feature.imageSrc}
@@ -188,10 +188,10 @@ const Features = () => {
                   </div>
 
                   <div
-                    className="flex items-end opacity-60 p-4 group-hover:opacity-100 transition ease-in-out delay-150 duration-300"
+                    className="flex opacity-70 p-4 group-hover:opacity-100 transition ease-in-out delay-150 duration-300"
                     aria-hidden="true"
                   >
-                    <div className=" items-center justify-cente text-base font-medium text-gray-700 z-10">
+                    <div className="items-center justify-cente text-base font-medium text-gray-700 z-10 dark:text-gray-200">
                       <h3 className="text-center">
                         <div className="font-bold">
                           <span
@@ -201,14 +201,15 @@ const Features = () => {
                           {feature.title}
                         </div>
                       </h3>
-                      <p className="text-sm text-center pt-2">
+                      <p className="text-sm text-center pt-2 dark:text-gray-400">
                         {feature.description}
                       </p>
                     </div>
                     {/* gradient for feature card */}
-                    <div className="absolute inset-x-0 bottom-0 rounded-lg h-1/2 bg-gradient-to-t from-gray-600 to-transparent opacity-40" />
+                    <div className="absolute inset-x-0 bottom-0 rounded-lg h-1/2 bg-gradient-to-t from-gray-600 to-transparent opacity-40 dark:from-secondary-gray500" />
                   </div>
                 </div>
+              </div>
               </div>
             ))}
           </div>
