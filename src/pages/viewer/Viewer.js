a11c202370f0159cc3a6b6f26ca567140722e81e
Suboda Dabarera <SubodaDabarera99@gmail.com>
2023-01-13 01:36:43 +0530

    color picker added

--- src/pages/viewer/Viewer.js
+++ src/pages/viewer/Viewer.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CursorArrowRaysIcon, MapPinIcon } from "@heroicons/react/24/solid";
 import { FaMousePointer } from "react-icons/fa";
 import { BsSlashLg } from "react-icons/bs";
 import { IoIosRedo, IoIosUndo } from "react-icons/io";
 import { IoColorPaletteOutline } from "react-icons/io5";
 import { TbRectangle, TbMinusVertical } from "react-icons/tb";
+import { CirclePicker } from "react-color";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -38,15 +39,35 @@ let toolbarIcons = [
 ];
 
 const Viewer = () => {
-  const [projectName, setProjectName] = useState("Cool Structures jkdjfkdf j");
+  const [projectName, setProjectName] = useState(
+    "Cool Structures jkdjfkdf endabcdefgh"
+  );
+  const [projectNameCompressed, setProjectNameCompressed] = useState("");
   const [rangeValue, setRangeValue] = useState(20);
-  const [isLineClicked, setIsLineClicked] = useState(true);
+  const [isLineClicked, setIsLineClicked] = useState(false);
   const [isMapPinClicked, setIsMapPinClicked] = useState(false);
   const [isCurserClicked, setIsCurserClicked] = useState(false);
   const [isBoxClicked, setIsBoxClicked] = useState(false);
   const [clickedItem, setClickedItem] = useState(
     localStorage.getItem("clickedItem")
   );
+  const [selectedColor, setSelectedColor] = useState("");
+  const [isColorPickerClicked, setIsColorPickerClicked] = useState(false);
+  const [isProjectNameHover, setIsProjectNameHover] = useState(false);
+
+  // check the length of project name
+  useEffect(() => {
+    console.log(projectName.length);
+    if (projectName.length > 32) {
+      const str = projectName;
+      const modStr = str.slice(0, 31) + " ...";
+      console.log(modStr);
+
+      setProjectNameCompressed(modStr);
+    } else {
+      setProjectNameCompressed(projectName);
+    }
+  }, []);
 
   const onItemClick = (item) => {
     setClickedItem(item.name);
@@ -69,6 +90,10 @@ const Viewer = () => {
       setIsLineClicked(false);
       setIsMapPinClicked(false);
     }
+  };
+
+  const onProjectNameHover = () => {
+    console.log(projectName);
   };
 
   return (
@@ -110,8 +135,8 @@ const Viewer = () => {
           </div>
 
           {/* second grid */}
-          <div className="grid items-center bg-red-400">
-            <div className="md:hidden grid grid-cols-2 items-center justify-between">
+          <div className="grid items-center">
+            <div className="md:hidden grid grid-cols-3 items-center justify-between">
               <div className="flex ml-3 gap-x-4 w-full justify-start">
                 <div>
                   <IoIosUndo className="h-6 w-6 text-yellow-900 dark:text-gray-200" />
@@ -120,13 +145,25 @@ const Viewer = () => {
                   <IoIosRedo className="h-6 w-6 text-yellow-900 dark:text-gray-200" />
                 </div>
               </div>
-              <div className="grid justify-end sm:mr-4 mr-2 py-1 dark:text-gray-200 ">
-                {projectName}
+              <div className="grid col-span-2 justify-end sm:mr-4 mr-2 py-1 dark:text-gray-200 ">
+                {projectNameCompressed}
               </div>
             </div>
-            <div className="hidden md:inline-flex py-1 dark:text-gray-200">
-              {projectName}
+
+            <div className="hidden relative md:inline-flex py-1 justify-center dark:text-gray-200">
+              <div
+                onMouseOver={() => setIsProjectNameHover(true)}
+                onMouseOut={() => setIsProjectNameHover(false)}
+              >
+                {projectNameCompressed}
+              </div>
             </div>
+
+            {/* {isProjectNameHover && (
+                <div className="origin absolute z-50 bg-red-400"> 
+                  called
+                </div>
+              )} */}
           </div>
 
           {/* third grid */}
@@ -168,9 +205,30 @@ const Viewer = () => {
 
             {isMapPinClicked && (
               <>
-                <div className="">
-                  <IoColorPaletteOutline className="w-6 h-6 text-gray-200" />
+                <div className="flex">
+                  {selectedColor.length > 0 ? (
+                    <TbRectangle
+                      color={selectedColor}
+                      fill={selectedColor}
+                      className="h-7 w-7"
+                      onClick={() => setIsColorPickerClicked((prev) => !prev)}
+                    />
+                  ) : (
+                    <IoColorPaletteOutline
+                      className="w-6 h-6 text-gray-300"
+                      onClick={() => setIsColorPickerClicked((prev) => !prev)}
+                    />
+                  )}
                 </div>
+
+                {isColorPickerClicked && (
+                  <div className="absolute origin-top-right top-28 p-3 rounded-lg dark:bg-secondary-gray700">
+                    <CirclePicker
+                      onChange={(color, event) => setSelectedColor(color.hex)}
+                      onChangeComplete={() => setIsColorPickerClicked(false)}
+                    />
+                  </div>
+                )}
               </>
             )}
           </div>
@@ -205,10 +263,34 @@ const Viewer = () => {
           {/* second grid */}
           <div className="flex justify-end items-center mr-4">
             {isMapPinClicked && (
-              <div className="">
-                <IoColorPaletteOutline className="w-6 h-6 text-gray-200" />
-              </div>
+              <>
+                <div className="flex">
+                  {selectedColor.length > 0 ? (
+                    <TbRectangle
+                      color={selectedColor}
+                      fill={selectedColor}
+                      className="h-7 w-7"
+                      onClick={() => setIsColorPickerClicked((prev) => !prev)}
+                    />
+                  ) : (
+                    <IoColorPaletteOutline
+                      className="w-6 h-6 text-gray-300"
+                      onClick={() => setIsColorPickerClicked((prev) => !prev)}
+                    />
+                  )}
+                </div>
+
+                {isColorPickerClicked && (
+                  <div className="absolute origin-bottom bottom-11 p-3 rounded-lg dark:bg-secondary-gray700">
+                    <CirclePicker
+                      onChange={(color, event) => setSelectedColor(color.hex)}
+                      onChangeComplete={() => setIsColorPickerClicked(false)}
+                    />
+                  </div>
+                )}
+              </>
             )}
+
             {isLineClicked && (
               <div className="">
                 <input
